68ccedefcdd1e2059034452a3811c1551cd7b951
Flobul <46993341+Flobul@users.noreply.github.com>
2023-11-03 09:12:45 +0100

    Configuration in js pure

--- desktop/js/configuration.js
+++ desktop/js/configuration.js
@@ -13,60 +13,36 @@
  * You should have received a copy of the GNU General Public License
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
- $('#bt_getCredentialsPlugin').on('click', function() {
-     if ($('.configKey[data-l1key="id"]').value() == '' || $('.configKey[data-l1key="password"]').value() == '') {
-         $.fn.showAlert({
-             message: '{{Veuillez entrer un identifiant et un mot de passe de connexion.}}',
-             level: 'danger'
-         });
-         return;
-     }
-     $.ajax({
-         type: "POST",
-         url: "plugins/lgthinq2/core/ajax/lgthinq2.ajax.php",
-         data: {
-             action: "getCredentials"
-         },
-         dataType: 'json',
-         error: function(request, status, error) {
-             handleAjaxError(request, status, error);
-         },
-         success: function(data) {
-             if (data.state != 'ok') {
-                 $.fn.showAlert({
-                     message: data.result,
-                     level: 'danger'
-                 });
-                 return;
-             }
-         }
-     });
- }
 
-   $(document).ready(function() {
-       var diff = $('.configKey[data-l1key=expires_in]').value() - Math.floor(Date.now() / 1000);
-       if (diff < 0) {
-           diff = '{{Expiré}}';
-           $('.configKey[data-l1key=expires_in]').addClass('dangerBgColor');
-       } else {
-           $('.configKey[data-l1key=expires_in]').removeClass('dangerBgColor');
-       }
-       $('.configKey[data-l1key=expires_in]').value(diff);
-       $('.configKey[data-l1key=expires_in]').removeClass('configKey').addClass('configKeyUnsaved')
-   });
+document.getElementById('bt_getCredentialsPlugin').addEventListener('click', function() {
+    var idInput = document.querySelector('.configKey[data-l1key="id"]');
+    var passwordInput = document.querySelector('.configKey[data-l1key="password"]');
 
-   // afficher juste avant la version, la véritable version contenue dans le plugin
-   var dateVersion = $("#span_plugin_install_date").html();
-   $("#span_plugin_install_date").empty().append("v" + version + " (" + dateVersion + ")");
+    if (idInput.value === '' || passwordInput.value === '') {
+        $.fn.showAlert({
+            message: '{{Veuillez entrer un identifiant et un mot de passe de connexion.}}',
+            level: 'danger'
+        });
+        return;
+    }
+});
 
-   $('.bt_refreshPluginInfo').after('<a class="btn btn-success btn-sm" target="_blank" href="https://market.jeedom.com/index.php?v=d&p=market_display&id=4099"><i class="fas fa-comment-dots "></i> Donner mon avis</a>');
+document.getElementById('div_plugin_configuration').addEventListener('change', function () {
+    var expiresInput = document.querySelector('.configKey[data-l1key="expires_in"]');
+    var currentTime = Math.floor(Date.now() / 1000);
 
-  $('.configKey[data-l1key=mobileormail]').off('change').on('change', function() {
-      if ($(this).value() == 1) {
-         $('.configKey[data-l1key=idemail]').hide();
-         $('.configKey[data-l1key=idmobile]').show();
-      } else {
-         $('.configKey[data-l1key=idemail]').show();
-         $('.configKey[data-l1key=idmobile]').hide();
-      }
-   });
+    if (expiresInput && expiresInput.value != '') {
+        var expiresValue = parseInt(expiresInput.value);
+
+        if (expiresValue < currentTime) {
+            expiresInput.value = '{{Expiré}}';
+            expiresInput.classList.add('dangerBgColor');
+        } else {
+            expiresInput.classList.remove('dangerBgColor');
+            expiresInput.value = expiresValue - currentTime;
+        }
+
+        expiresInput.classList.remove('configKey');
+        expiresInput.classList.add('configKeyUnsaved');
+    }
+});
